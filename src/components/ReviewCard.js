cd2f787bcaa05dcb319e846bea8c1f006d04e6fd
Kristinna97 <kristina.petrova13@gmail.com>
2022-06-14 12:04:57 +0100

    add style on card

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -3,9 +3,13 @@ const ReviewCard = ({review}) =>{
     
     return(
         <div className={styles.review_card}>
-            <h3 className={styles.title}>{review.title}</h3>
+           
             <img className={styles.review_img} src={review.review_img_url} alt={review.title}></img>
-             <p className={styles.review_info}><span>{(new Date(review.created_at)).toLocaleDateString('en-GB')}</span><span> Likes: {review.votes}</span> </p>
+             
+             <p className={styles.text_block}>
+                 <span className={styles.title}>{review.title}</span>
+                <span>{(new Date(review.created_at)).toLocaleDateString('en-GB')}</span><span> Likes: {review.votes}</span>
+                 </p>
            
         </div>
     )
