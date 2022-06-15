dfb88172f214a8155b45a1b197ab445468fcf110
Kristinna97 <kristina.petrova13@gmail.com>
2022-06-15 11:36:39 +0100

    display msg if there is no comments on review

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -5,7 +5,7 @@ import styles from './CommentList.module.css'
 
 const CommentsList = ({review_id}) => {
 
-    const [comments, setComments] = useState()
+    const [comments, setComments] = useState([])
 
     useEffect(() =>{
         fetchComments(review_id).then((data) => {
@@ -13,7 +13,9 @@ const CommentsList = ({review_id}) => {
         })
     },[review_id])
     
- if(comments){
+ if(comments.length === 0 ){
+    return <h4 className={styles.noComments}>No comments for this post yet :( </h4>
+ }else{
     return <> <ul className={styles.comment_list}>
     {comments.map((comment )=> {
   return  <SingleComment comment={comment} key={comment.body}/>
